8f85db57e4f53f1de4de4c1525860d0c70b0d1f7
Anthony_Desktop <kalongprojectuse.gmail.com>
2023-01-29 02:14:21 +0800

    added s3 setup

--- terraform_setup_ec2/main.tf
+++ terraform_setup_ec2/main.tf
@@ -1,23 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "testing_server"
   }
+}
+
+resource "aws_s3_bucket" "tests3"{
+for_each = toset(var.bucket_name)
+bucket = each.key
+
+tags ={
+Name = "testing bucket"
+Environment = "Dev"
+	}
 }
