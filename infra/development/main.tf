b50d92b27eaec3f162000078123eb0218986c31a
Anthony_Desktop <kalongprojectuse.gmail.com>
2023-05-28 21:37:42 +0800

    add iam module, for creating multiple users and assess key and add these people into specific groups

--- infra/development/main.tf
+++ infra/development/main.tf
@@ -1,7 +1,11 @@
-module "ec2-instance"{
-    source = "../modules/ec2"
-}
 
+module "iam" {
+    source = "../modules/iam"
+}
 module "s3" {
     source = "../modules/s3"
 }
+
+#module "ec2-instance"{
+#    source = "../modules/ec2"
+#}
--- infra/development/provider.tf
+++ infra/development/provider.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-	access_key = "${var.aws_access_key}"
-	secret_key = "${var.aws_secret_key}"
-	region = "${var.region}" 
+	region = "${var.region}"
+  access_key = "AKIAQJI4UGD3BGE7XLB3"
+  secret_key = "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"
 }
